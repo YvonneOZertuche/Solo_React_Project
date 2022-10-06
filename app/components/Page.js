86c6314e93ebbf2317c24f4aa40343dc2f94aabf
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-10-06 06:54:35 -0500

    View Single Post functionality completed

--- app/components/Page.js
+++ app/components/Page.js
@@ -5,7 +5,7 @@ const Page = (props) => {
    useEffect(() => {
      document.title = `${props.title} | MyBrokenApp`
      window.scrollTo(0, 0)
-   }, [])
+   }, [props.title])
   return (
    <Container wide={props.wide}>
       {props.children}
